6633cfac63b13deb1ad695b246ca2b57b6131ef3
Bradleyking4 <bradleyking4@dodo.com.au>
2018-03-12 13:15:06 +1000

    c# mqtt client added

---
+++ base_station/BaseStation/BaseStation/Form1.Designer.cs
@@ -0,0 +1,141 @@
+﻿namespace BaseStation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.tbxMessage = new System.Windows.Forms.TextBox();
+            this.rtbSubscribe = new System.Windows.Forms.RichTextBox();
+            this.tbxAddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxTopic = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(831, 90);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(650, 451);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Send";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // tbxMessage
+            // 
+            this.tbxMessage.Location = new System.Drawing.Point(487, 454);
+            this.tbxMessage.Name = "tbxMessage";
+            this.tbxMessage.Size = new System.Drawing.Size(160, 20);
+            this.tbxMessage.TabIndex = 2;
+            // 
+            // rtbSubscribe
+            // 
+            this.rtbSubscribe.Location = new System.Drawing.Point(182, 225);
+            this.rtbSubscribe.Name = "rtbSubscribe";
+            this.rtbSubscribe.Size = new System.Drawing.Size(462, 172);
+            this.rtbSubscribe.TabIndex = 3;
+            this.rtbSubscribe.Text = "";
+            // 
+            // tbxAddress
+            // 
+            this.tbxAddress.Location = new System.Drawing.Point(199, 92);
+            this.tbxAddress.Name = "tbxAddress";
+            this.tbxAddress.Size = new System.Drawing.Size(462, 20);
+            this.tbxAddress.TabIndex = 4;
+            this.tbxAddress.Text = "192.168.20.151";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(484, 438);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Message";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(182, 435);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(34, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Topic";
+            // 
+            // tbxTopic
+            // 
+            this.tbxTopic.Location = new System.Drawing.Point(182, 454);
+            this.tbxTopic.Name = "tbxTopic";
+            this.tbxTopic.Size = new System.Drawing.Size(266, 20);
+            this.tbxTopic.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(965, 513);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbxTopic);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxAddress);
+            this.Controls.Add(this.rtbSubscribe);
+            this.Controls.Add(this.tbxMessage);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox tbxMessage;
+        private System.Windows.Forms.RichTextBox rtbSubscribe;
+        private System.Windows.Forms.TextBox tbxAddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxTopic;
+    }
+}
+
---
+++ base_station/BaseStation/BaseStation/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace BaseStation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // create client instance 
+            MqttClient client = new MqttClient(IPAddress.Parse(tbxAddress.Text));
+
+            // register to message received 
+            client.MqttMsgPublishReceived += client_MqttMsgPublishReceived;
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            // subscribe to the topic "/home/temperature" with QoS 2 
+            client.Subscribe(new string[] { "#" }, new byte[] { MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE });
+
+
+        }
+
+
+        delegate void SetTextCallback(string text);
+
+        private void SetText(string text)
+        {
+            // InvokeRequired required compares the thread ID of the
+            // calling thread to the thread ID of the creating thread.
+            // If these threads are different, it returns true.
+            if (this.rtbSubscribe.InvokeRequired)
+            {
+                SetTextCallback d = new SetTextCallback(SetText);
+                this.Invoke(d, new object[] { text });
+            }
+            else
+            {
+                this.rtbSubscribe.AppendText(text);
+            }
+        }
+
+        void client_MqttMsgPublishReceived(object sender, MqttMsgPublishEventArgs e)
+        {
+            SetText(e.Topic + " ");
+            SetText(Encoding.UTF8.GetString(e.Message,0, e.Message.Length) + Environment.NewLine);
+
+
+            // handle message received 
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            // create client instance 
+            MqttClient client = new MqttClient(IPAddress.Parse(tbxAddress.Text));
+
+            string clientId = Guid.NewGuid().ToString();
+            client.Connect(clientId);
+
+            string strValue = Convert.ToString(tbxMessage.Text);
+
+            // publish a message on "/home/temperature" topic with QoS 2 
+            client.Publish(tbxTopic.Text, Encoding.UTF8.GetBytes(strValue), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+
+
+        }
+
+
+    }
+}
