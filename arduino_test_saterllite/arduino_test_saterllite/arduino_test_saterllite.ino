84e37ab042b2da2694f9d2fa3635f81195f1fd6d
Bradleyking4 <bradleyking4@dodo.com.au>
2018-03-13 12:06:05 +1000

    add RGB led for testing

--- arduino_test_saterllite/arduino_test_saterllite/arduino_test_saterllite.ino
+++ arduino_test_saterllite/arduino_test_saterllite/arduino_test_saterllite.ino
@@ -21,6 +21,13 @@
   http://www.arduino.cc/en/Tutorial/PhysicalPixel
 */
 
+struct RGB {
+  byte value[3];
+  byte pin[3];
+};
+
+struct RGB rgb1, rgb2;
+
 const int ledPin = 13; // the pin that the LED is attached to
 int incomingByte;      // a variable to read incoming serial data into
 
@@ -29,6 +36,18 @@ void setup() {
   Serial.begin(9600);
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
+
+  rgb2.pin[0] = 6;
+  rgb2.pin[1] = 7;
+  rgb2.pin[2] = 9;
+
+  rgb1.pin[0] = A0;
+  rgb1.pin[1] = A1;
+  rgb1.pin[2] = A3;
+  for (int i = 0; i < 3; i++) {
+    pinMode(rgb2.pin[i], OUTPUT);
+    pinMode(rgb1.pin[i], OUTPUT);
+  }
 }
 
 unsigned long lastSendTime;
@@ -59,6 +78,32 @@ void loop() {
       if (readLine.charAt(payloadPosition + 1) == 'L') {
         digitalWrite(ledPin, LOW);
       }
+    }
+
+    if (readLine.startsWith("rgb1")) {
+      payloadPosition =  readLine.indexOf(":");
+      // if it's a capital H (ASCII 72), turn on the LED:
+      for (int i = 0; i < 3; i++) {
+        if (readLine.charAt(payloadPosition + 1+ i) == 'H') {
+          digitalWrite(rgb1.pin[i], HIGH);
+        } else {
+          digitalWrite(rgb1.pin[i], LOW);
+        }
+      }
+
+    }
+    
+    if (readLine.startsWith("rgb2")) {
+      payloadPosition =  readLine.indexOf(":");
+      // if it's a capital H (ASCII 72), turn on the LED:
+      for (int i = 0; i < 3; i++) {
+        if (readLine.charAt(payloadPosition +1 + i) == 'H') {
+          digitalWrite(rgb2.pin[i], HIGH);
+        } else {
+          digitalWrite(rgb2.pin[i], LOW);
+        }
+      }
+
     }
   }
 }
