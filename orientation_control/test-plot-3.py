396e2657301fdcec4634991b255a5584a5e58efe
Roland Thompson <rolanditaru@gmail.com>
2018-03-17 19:12:36 +1000

    Hackflight attempt

--- orientation_control/test-plot-3.py
+++ orientation_control/test-plot-3.py
@@ -4,22 +4,25 @@ matplotlib.use("qt4agg")
 
 """test-imu-plot.py: Ask multiwii for raw IMU and plot it using matplotlib."""
 
-__author__ = "Aldo Vargas"
-__copyright__ = "Copyright 2016 Altax.net"
-
-__license__ = "GPL"
-__version__ = "1"
-__maintainer__ = "Aldo Vargas"
-__email__ = "alduxvm@gmail.com"
-__status__ = "Development"
-
+import sys
+import signal
 from pymultiwii import MultiWii
 import matplotlib.pyplot as plt
 
 serialPort = "COM4"
 board = MultiWii(serialPort)
 
+def handle_close(evt):
+    sys.exit()
+    
+def handle_ctrlC(signal, frame):
+    print 'You pressed Ctrl+C!'
+    sys.exit()
+    
+signal.signal(signal.SIGINT, handle_ctrlC)
+
 fig = plt.figure()
+fig.canvas.mpl_connect('close_event', handle_close)
 
 sb = 0
 gxbuf, gybuf, gzbuf = 0, 0, 0
